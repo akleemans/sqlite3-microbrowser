a2c6c1d3f80a4e2e43a63316c556fe0743a8c1cc
Adrianus Kleemans <a.kleemans@gmail.com>
2016-04-25 21:47:04 +0200

    Tabs with Angular material
    Added Todo list

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 def index():
     return 'Flask is running!'
 
-
 @app.route('/data')
 def names():
     data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
     return jsonify(data)
 
 if __name__ == '__main__':
+    # TODO discover db in directory
+    # TODO analyze tables (.tables)
+    # TODO run .schema for each table
+    # TODO serve initial data
     app.run()
